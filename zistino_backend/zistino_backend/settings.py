b758edf3eb8d5c6f03091ad57121098362088a36
softmiliac <110964505+softmiliac@users.noreply.github.com>
2025-11-16 16:59:38 +0330

    Add production URL to allowed CORS origins

--- zistino_backend/zistino_backend/settings.py
+++ zistino_backend/zistino_backend/settings.py
@@ -217,6 +217,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:8080",
     "http://localhost:8000",
     "http://127.0.0.1:8000",
+    "https://zistino-latest.onrender.com",
 ]
 
 # Allow all origins in development (for Swagger UI and testing)
